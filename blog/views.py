16ddd8248f324ac622bf5f12ac71d934d7e8d836
Anton <ryazanova.anjelika@gmail.com>
2018-04-06 21:43:22 +0300

    Added CSS colou24r

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list1.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail1.html', {'post': post})
